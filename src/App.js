656d6cbb195dfd4bb2f066bd9cfb1bae3bcc9600
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-09-21 11:56:16 -0400

    Implement routing and import statements

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./pages/Home";
+
+// Create a router configuration using createBrowserRouter.
+// This configuration specifies that the Home component should be rendered when the path is "/".
+const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
+
+// Define the main App component.
 function App() {
-  return <div></div>;
+  // Return the RouterProvider component with the router configuration as a prop.
+  // This allows the routing functionality to be available throughout the app.
+  return <RouterProvider router={router} />;
 }
 
 export default App;
